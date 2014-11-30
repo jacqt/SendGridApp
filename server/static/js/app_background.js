f0025c753b5be2807731a6ea5daa616d3656f641
Anthony Guo <anthony.guo@some.ox.ac.uk>
2014-11-30 10:39:02 +0000

    merged some changes in

--- server/static/js/app_background.js
+++ server/static/js/app_background.js
@@ -9,13 +9,13 @@ var Utils =  {
         ctx.save();
         ctx.strokeStyle = "rgba(250,250,250,0.7)";
         ctx.beginPath();
-        ctx.moveTo(0, height/2);
-        ctx.lineTo(width, height/2);
+        ctx.moveTo(50, height/2);
+        ctx.lineTo(width - 50, height/2);
         ctx.stroke();
 
         ctx.beginPath();
-        ctx.moveTo(width/2, 0);
-        ctx.lineTo(width/2, height);
+        ctx.moveTo(width/2, 80);
+        ctx.lineTo(width/2, height-50);
         ctx.stroke();
 
         ctx.beginPath();
@@ -33,7 +33,7 @@ var Utils =  {
         ctx.stroke();
 
         gameModel.drawString("45°", width/2 + 50, height/2 - 100, 0, "center", "white", false, "20px Verdana");
-        gameModel.drawString("0°", width/2 + 20, 70, 0, "center", "white", false, "20px Verdana");
+        gameModel.drawString("0°", width/2 + 20, 110, 0, "center", "white", false, "20px Verdana");
         ctx.restore();
 
     }
